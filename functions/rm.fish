446d9a29ade3751ad515fd791084673533c2c36c
mattmc3 <mattmc3@gmail.com>
2019-07-30 23:48:36 -0400

    Fix

--- functions/rm.fish
+++ functions/rm.fish
@@ -1,3 +1,3 @@
-function rm --description 'A safer rm'
-    perl -t "(dirname (status -f))"/__safe-rm.pl $argv
+function rm2 --description 'A safer rm'
+    perl -t (dirname (status -f))/__safe-rm.pl $argv
 end
